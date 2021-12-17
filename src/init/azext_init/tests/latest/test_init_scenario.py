cb5879543e02932e776ef6035526fdb4fd0637e7
zhoxing-ms <Zhou.Xing@microsoft.com>
2021-12-17 15:13:23 +0800

    Remove unused import

--- src/init/azext_init/tests/latest/test_init_scenario.py
+++ src/init/azext_init/tests/latest/test_init_scenario.py
@@ -4,10 +4,8 @@
 # --------------------------------------------------------------------------------------------
 
 import os
-import unittest
 
-from azure_devtools.scenario_tests import AllowLargeResponse
-from azure.cli.testsdk import (ScenarioTest, ResourceGroupPreparer)
+from azure.cli.testsdk import (ScenarioTest)
 
 
 TEST_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), '..'))
